ae067d60b71778ecc3b0199e607c43a988ec9333
MuthukumaranaSM <sandanimuthukumarana@gmail.com>
2025-01-18 13:39:58 +0530

    create slidebar as seperate component

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,81 +1,17 @@
 import React from "react";
-import clock from "./images/clock.svg";
-import cloud from "./images/cloud.svg";
-import downloadCloud from "./images/download-cloud.svg";
-import file from "./images/file.png";
-import folder from "./images/folder.svg";
-import image from "./images/image.svg";
-import line2 from "./images/line-2.svg";
-import logOut from "./images/log-out.svg";
+import { Sidebar } from "./Sidebar"; // Import the Sidebar component
 import rectangle1 from "./images/rectangle-1.svg";
 import search from "./images/search.svg";
-import star from "./images/star.png";
+import image from "./images/image.svg";
+import file from "./images/file.png";
 import youtube from "./images/youtube.svg";
+import downloadCloud from "./images/download-cloud.svg";
 import "./style.css";
 
 export const Dashboard = () => {
   return (
     <div className="dashboard">
-      <aside className="sidebar">
-        <div className="sidebar-header">
-          <h1 className="sidebar-title">My Docs</h1>
-          <img className="line" alt="Separator line" src={line2} />
-        </div>
-        <nav className="sidebar-menu">
-          <div className="menu-section">
-            <ul className="menu-list">
-              {/* Home as a tab */}
-              <a href="#Home">
-                <li className="menu-item">
-                  <img className="icon" alt="Home" src={cloud} />
-                  Home
-                </li>
-              </a>
-            </ul>
-          </div>
-          <div className="menu-section">
-            <h2 className="menu-title">File Manager</h2>
-            <ul className="menu-list">
-              <a href="#MyStorage">
-                <li className="menu-item">
-                  <img className="icon" alt="Cloud storage" src={cloud} />
-                  My Storage
-                </li>
-              </a>
-              <a href="#Recent">
-                <li className="menu-item">
-                  <img className="icon" alt="Recent files" src={star} />
-                  Recent
-                </li>
-              </a>
-              <a href="#Favourite">
-                <li className="menu-item">
-                  <img className="icon" alt="Favourite files" src={clock} />
-                  Favourite
-                </li>
-              </a>
-            </ul>
-          </div>
-          <div className="menu-section">
-            <h2 className="menu-title">Shared File</h2>
-            <ul className="menu-list">
-              <a href="#SharedFolder">
-                <li className="menu-item">
-                  <img className="icon" alt="Shared folder" src={folder} />
-                  Shared Folder
-                </li>
-              </a>
-              <a href="#SharedDocument">
-                <li className="menu-item">
-                  <img className="icon" alt="Shared document" src={file} />
-                  Shared File
-                </li>
-              </a>
-            </ul>
-          </div>
-        </nav>
-        <img className="log-out" alt="Log out button" src={logOut} />
-      </aside>
+      <Sidebar /> {/* Use the Sidebar component here */}
       <main className="main-content">
         <header className="main-header">
           <img className="search-bar" alt="Search bar" src={rectangle1} />
---
+++ frontend/src/Sidebar.js
@@ -0,0 +1,73 @@
+import React from "react";
+import clock from "./images/clock.svg";
+import cloud from "./images/cloud.svg";
+import file from "./images/file.png";
+import folder from "./images/folder.svg";
+import line2 from "./images/line-2.svg";
+import logOut from "./images/log-out.svg";
+import star from "./images/star.png";
+import "./style.css";
+
+export const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <h1 className="sidebar-title">My Docs</h1>
+        <img className="line" alt="Separator line" src={line2} />
+      </div>
+      <nav className="sidebar-menu">
+        <div className="menu-section">
+          <ul className="menu-list">
+            <a href="#Home">
+              <li className="menu-item">
+                <img className="icon" alt="Home" src={cloud} />
+                Home
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div className="menu-section">
+          <h2 className="menu-title">File Manager</h2>
+          <ul className="menu-list">
+            <a href="#MyStorage">
+              <li className="menu-item">
+                <img className="icon" alt="Cloud storage" src={cloud} />
+                My Storage
+              </li>
+            </a>
+            <a href="#Recent">
+              <li className="menu-item">
+                <img className="icon" alt="Recent files" src={star} />
+                Recent
+              </li>
+            </a>
+            <a href="#Favourite">
+              <li className="menu-item">
+                <img className="icon" alt="Favourite files" src={clock} />
+                Favourite
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div className="menu-section">
+          <h2 className="menu-title">Shared File</h2>
+          <ul className="menu-list">
+            <a href="#SharedFolder">
+              <li className="menu-item">
+                <img className="icon" alt="Shared folder" src={folder} />
+                Shared Folder
+              </li>
+            </a>
+            <a href="#SharedDocument">
+              <li className="menu-item">
+                <img className="icon" alt="Shared document" src={file} />
+                Shared File
+              </li>
+            </a>
+          </ul>
+        </div>
+      </nav>
+      <img className="log-out" alt="Log out button" src={logOut} />
+    </aside>
+  );
+};
