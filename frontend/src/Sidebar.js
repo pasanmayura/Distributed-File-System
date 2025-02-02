e0992b6071d61563ab20c32eddd30544285ab64a
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2025-02-02 21:18:33 +0530

    enhance Sidebar component with new icons and improved navigation structure

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,18 +1,24 @@
 import React from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import CloudIcon from "@mui/icons-material/Cloud";
-import HistoryIcon from "@mui/icons-material/History";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LogoutIcon from "@mui/icons-material/Logout";
+import { Link } from 'react-router-dom';
+import clock from "./images/clock.svg";
+import cloud from "./images/cloud.svg";
+import file from "./images/file.png";
+import folder from "./images/folder.svg";
 import line2 from "./images/line-2.svg";
+import logOut from "./images/log-out.svg";
+import star from "./images/star.png";
 import "./style.css";
 
 export const Sidebar = () => {
+
   const userEmail = sessionStorage.getItem("userEmail");
 
   const handleLogout = () => {
+    // Clear session storage
     sessionStorage.removeItem("userEmail");
-    window.location.href = "/login";
+    
+    // Redirect to login page
+    window.location.href = "/login";  // Or use history.push('/login') if you're using React Router
   };
 
   return (
@@ -22,39 +28,66 @@ export const Sidebar = () => {
         <h4>{userEmail ? userEmail : "User"}</h4>
         <img className="line" alt="Separator line" src={line2} />
       </div>
-
       <nav className="sidebar-menu">
-        <ul className="menu-list">
-          <a href="#Home">
+        <div className="menu-section">
+          <ul className="menu-list">
+            <a href="#Home">
+              <li className="menu-item">
+                <img className="icon" alt="Home" src={cloud} />
+                Home
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div className="menu-section">
+          <h2 className="menu-title">File Manager</h2>
+          <ul className="menu-list">
+          <Link to="/storage">
             <li className="menu-item">
-              <HomeIcon className="icon" />
-              Home
-            </li>
-          </a>
-          <a href="#MyStorage">
-            <li className="menu-item">
-              <CloudIcon className="icon" />
+              <img className="icon" alt="Cloud storage" src={cloud} />
               My Storage
             </li>
-          </a>
-          <a href="#Recent">
-            <li className="menu-item">
-              <HistoryIcon className="icon" />
-              Recent
-            </li>
-          </a>
-          <a href="#Favourite">
-            <li className="menu-item">
-              <FavoriteIcon className="icon" />
-              Favourite
-            </li>
-          </a>
-        </ul>
+          </Link>
+            <a href="#Recent">
+              <li className="menu-item">
+                <img className="icon" alt="Recent files" src={star} />
+                Recent
+              </li>
+            </a>
+            <a href="#Favourite">
+              <li className="menu-item">
+                <img className="icon" alt="Favourite files" src={clock} />
+                Favourite
+              </li>
+            </a>
+          </ul>
+        </div>
+        <div className="menu-section">
+          <h2 className="menu-title">Shared File</h2>
+          <ul className="menu-list">
+            <a href="#SharedFolder">
+              <li className="menu-item">
+                <img className="icon" alt="Shared folder" src={folder} />
+                Shared Folder
+              </li>
+            </a>
+            <a href="#SharedDocument">
+              <li className="menu-item">
+                <img className="icon" alt="Shared document" src={file} />
+                Shared File
+              </li>
+            </a>
+          </ul>
+        </div>
       </nav>
 
-      <div className="logout-container">
-        <LogoutIcon className="log-out" onClick={handleLogout} />
-      </div>
+      <img 
+        className="log-out" 
+        alt="Log out button" 
+        src={logOut} 
+        onClick={handleLogout} 
+      />
+
     </aside>
   );
 };
